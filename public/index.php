539e01cf2e840292d504abef4c9595779b27d448
Lorena Helmers <lorenahelmers@dhcp-108.intern.2do-werbeagentur.de>
2018-07-11 11:30:02 +0200

    hallo

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="http://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+
+
+
+  <link rel="stylesheet" href="assets/css/main.min.css">
+  <title>Titel</title>
+</head>
+
+<body>
+  <h1>Hier steht der Titel</h1>
+  <button>Klick mich</button>
+
+
+
+
+  <script src="assets/js/vendors.min.js"></script>
+  <script src="assets/js/main.min.js"></script>
+
+</body>
+
+</html>
